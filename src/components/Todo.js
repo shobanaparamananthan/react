89a52f8d86847096acb381ae24d04efd42cbf461
shobanaparamananthan <43436669+shobanaparamananthan@users.noreply.github.com>
2021-06-28 12:25:28 +0530

    commit todo list app

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Todo({ todo}){
+    return (
+        <div style={{display :"flex"}}>
+    <input type ="checkbox"/>
+    <li
+    style={{
+        color:"white"
+       
+    }}>{todo.task}</li>
+    <button>X</button>
+    </div>
+    );
+}
+export default Todo;
